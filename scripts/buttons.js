59502a17ab0c5a69a73face0d015fb0a76fe183c
Elizabeth Labor <em.labor@nyu.edu>
2020-08-16 15:02:49 -0500

    Remove now ignored cypress examples
    Also gets rid of some files I accidentally created in this branch (buttons.js and popup.js)

--- scripts/buttons.js
+++
@@ -1,9 +0,0 @@
-	
-function editWhyText(){
-	sidebarBody().find("#editSubgoal").hide();
-	sidebarBody().find('#editSubName').hide();
-	sidebarBody().find('#addAction').hide();
-	sidebarBody().find("#A0Q0whyYes").show();
-	sidebarBody().find("#A0Q0whyYes").html(subgoal.why);
-	sidebarBody().find('#submitWhy').show();
-}
